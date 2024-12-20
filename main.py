18e74a59f714d6cc1242710791e69c1276c8bf37
Tenakskd <anzuday7777@gmail.com>
2024-12-20 15:58:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -107,11 +107,13 @@ def get_data(videoid):
 
 def getting_data(videoid):
     urls = [
+        f"https://ytserver1.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://ludicrous-wonderful-temple.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://free-sudden-kiss.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://https://wataamee.glitch.me/api/{urllib.parse.quote(videoid)}",
         f"https://natural-voltaic-titanium.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://jade-highfalutin-account.glitch.me/api/login/{urllib.parse.quote(videoid)}"
+        
     ]
     for url in urls:
         response = requests.get(url)
