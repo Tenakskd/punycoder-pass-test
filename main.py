1a670cb32e9c75f72c40ad08b21b0456c4dd6eae
Tenakskd <anzuday7777@gmail.com>
2024-12-22 11:12:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -108,9 +108,11 @@ def get_data(videoid):
 def getting_data(videoid):
     urls = [
         f"https://just-frequent-network.glitch.me/api/{urllib.parse.quote(videoid)}",
+        f"https://amenable-charm-lute.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://ludicrous-wonderful-temple.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://free-sudden-kiss.glitch.me/api/login/{urllib.parse.quote(videoid)}",
-        f"https://https://wataamee.glitch.me/api/{urllib.parse.quote(videoid)}",
+        f"https://wtserver1.glitch.me/api/login/{urllib.parse.quote(videoid)}",
+        f"https://wataamee.glitch.me/api/{urllib.parse.quote(videoid)}",
         f"https://natural-voltaic-titanium.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://jade-highfalutin-account.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://watawatawata.glitch.me/api/login/{urllib.parse.quote(videoid)}"
