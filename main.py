7fd8d803393b51869b3bc980cb615cedeb068d00
Tenakskd <anzuday7777@gmail.com>
2024-12-29 11:14:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -108,6 +108,7 @@ def get_data(videoid):
 def getting_data(videoid):
     urls = [
         f"https://just-frequent-network.glitch.me/api/{urllib.parse.quote(videoid)}",
+        f"https://wakame02o.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://amenable-charm-lute.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://ludicrous-wonderful-temple.glitch.me/api/login/{urllib.parse.quote(videoid)}",
         f"https://free-sudden-kiss.glitch.me/api/login/{urllib.parse.quote(videoid)}",
